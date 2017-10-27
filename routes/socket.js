74f8b77614f398b8e3c26aac7017401f9d875d13
vigneshmack <vignesh.mack03@gmail.com>
2017-10-27 22:57:26 +0530

    Revert "profile upload ..."

--- routes/socket.js
+++ routes/socket.js
@@ -2,8 +2,14 @@
 
 module.exports=function(io){
 
+
     io.on("connection",function(socket){
         console.log("A user connected:"+socket.id);
-        socket.emit('welcome_message', 'Welcome to YIC');
     });
+
+
+
+
+
 };
+
